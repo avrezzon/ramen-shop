fc30c3613c3ea81106af1250e14ed563af474ea1
Anthony Rezzonico <anthonyrezz@gmail.com>
2023-05-04 16:33:26 -0600

    Created basic unit tests for POST PUT and DELETE

--- server/src/test/java/com/ramenshop/server/service/RamenServiceTest.java
+++ server/src/test/java/com/ramenshop/server/service/RamenServiceTest.java
@@ -2,6 +2,7 @@ package com.ramenshop.server.service;
 
 import com.ramenshop.server.converter.MenuItemConverter;
 import com.ramenshop.server.converter.RamenConverter;
+import com.ramenshop.server.dto.MenuItemDto;
 import com.ramenshop.server.exception.RamenNotFoundException;
 import com.ramenshop.server.model.Measurement;
 import com.ramenshop.server.model.Ramen;
@@ -19,6 +20,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -35,19 +38,32 @@ public class RamenServiceTest {
     public static final double PRICE = 12.59;
     public static final String MENU_CODE = "1A";
 
-    private final Ramen entry = Ramen.builder()
-            .name(NAME)
-            .menuCode(MENU_CODE)
-            .recipe(List.of(
-                    new Serving("Pork bone broth", 3.0, Measurement.cup),
-                    new Serving("Noodles", 1.0, Measurement.lbs),
-                    new Serving("Roasted pork", 0.5, Measurement.lbs)
-            ))
-            .price(PRICE)
-            .build();
+    private Ramen entry;
+    private MenuItemDto menuItem;
 
     @BeforeEach
-    void init(){
+    void init() {
+        this.entry = Ramen.builder()
+                .name(NAME)
+                .menuCode(MENU_CODE)
+                .recipe(List.of(
+                        new Serving("Pork bone broth", 3.0, Measurement.cup),
+                        new Serving("Noodles", 1.0, Measurement.lbs),
+                        new Serving("Roasted pork", 0.5, Measurement.lbs)
+                ))
+                .price(PRICE)
+                .build();
+
+        this.menuItem = MenuItemDto.builder()
+                .name(NAME)
+                .ingredients(List.of(
+                        new Serving("Pork bone broth", 3.0, Measurement.cup),
+                        new Serving("Noodles", 1.0, Measurement.lbs),
+                        new Serving("Roasted pork", 0.5, Measurement.lbs)
+                ))
+                .suggestedPrice(PRICE)
+                .build();
+
         this.conversionService = new GenericConversionService();
         this.conversionService.addConverter(new RamenConverter());
         this.conversionService.addConverter(new MenuItemConverter());
@@ -56,20 +72,20 @@ public class RamenServiceTest {
     }
 
     @Test
-    void findAllRamenOfferings_EmptyCollection(){
+    void findAllRamenOfferings_EmptyCollection() {
         when(ramenRepository.findAll()).thenReturn(Collections.emptyList());
         var result = service.findAllRamenOfferings();
         assertTrue(result.isEmpty());
     }
 
     @Test
-    void findAllRamenOfferings_NonEmptyCollection(){
+    void findAllRamenOfferings_NonEmptyCollection() {
         when(ramenRepository.findAll()).thenReturn(List.of(entry));
 
         var result = service.findAllRamenOfferings();
 
-        assertFalse( result.isEmpty());
-        assertEquals(1 , result.size());
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
 
         var foundEntry = result.get(0);
         assertEquals(NAME, foundEntry.getName());
@@ -77,18 +93,60 @@ public class RamenServiceTest {
     }
 
     @Test
-    void findRamenByMenuCode_Success(){
+    void findRamenByMenuCode_Success() {
         when(ramenRepository.findById(MENU_CODE)).thenReturn(Optional.of(entry));
         var result = service.findRamenByMenuCode(MENU_CODE);
         assertNotNull(result);
     }
 
     @Test
-    void findRamenByMenuCode_NotFound(){
+    void findRamenByMenuCode_NotFound() {
         when(ramenRepository.findById(MENU_CODE)).thenReturn(Optional.empty());
         assertThrows(RamenNotFoundException.class, () -> {
             service.findRamenByMenuCode(MENU_CODE);
         });
     }
 
+    @Test
+    void createNewMenuItem_Success() {
+        when(ramenRepository.save(any(Ramen.class))).thenReturn(entry);
+        service.createNewMenuItem(menuItem);
+        verify(ramenRepository).save(any(Ramen.class));
+    }
+
+    @Test
+    void updateMenuItem_ItemDoesntExist() {
+        assertThrows(RamenNotFoundException.class, () -> {
+            service.updateMenuItem(MENU_CODE, menuItem);
+        });
+    }
+
+    @Test
+    void updateMenuItem_Sucess() {
+        when(ramenRepository.findById(MENU_CODE)).thenReturn(Optional.ofNullable(entry.toBuilder()
+                .name("Old name")
+                .price(999.99)
+                .build()));
+        when(ramenRepository.save(any())).thenReturn(entry);
+
+        var result = service.updateMenuItem(MENU_CODE, menuItem);
+
+        assertEquals(MENU_CODE, result.getMenuCode());
+        assertEquals(NAME, result.getName());
+        assertEquals(PRICE, result.getPrice());
+    }
+
+    @Test
+    void deleteMenuItem_Success(){
+        when(ramenRepository.findById(MENU_CODE)).thenReturn(Optional.ofNullable(entry));
+        service.deleteMenuItem(MENU_CODE);
+        verify(ramenRepository).delete(any());
+    }
+
+    @Test
+    void deleteMenuItem_ItemDoesntExist(){
+        assertThrows(RamenNotFoundException.class, () -> {
+            service.deleteMenuItem(MENU_CODE);
+        });
+    }
 }
